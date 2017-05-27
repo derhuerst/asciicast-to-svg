08cebf1f707a2962324dac3b32f6bc17ecf6bc84
Jannis R <mail@jannisr.de>
2017-05-27 12:48:20 +0200

    implementation :clap:, test :white_check_mark:

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 'use strict'
 
-// todo
+const Terminal = require('headless-terminal')
+
+const createRenderer = (meta) => {
+	const t = new Terminal(meta.width, meta.height)
+
+	const render = (diff) => {
+		t.write(diff)
+		const text = t.displayBuffer.toString()
+		return text
+	}
+
+	return render
+}
+
+module.exports = {createRenderer}
--- test.js
+++ test.js
@@ -2,8 +2,44 @@
 
 const test = require('tape')
 
-const createRenderer = require('.')
+const {createRenderer} = require('.')
+
+const asciicast = {
+	version: 1,
+	width: 12,
+	height: 2,
+	duration: .6,
+	command: 'bash',
+	title: '',
+	env: {
+		TERM: 'xterm-256color',
+		SHELL: '/bin/bash'
+	},
+	stdout: [
+		[.01, '\u001b[1;31mH'],
+		[.02, 'e'],
+		[.03, 'l'],
+		[.04, 'l'],
+		[.05, 'o'],
+		[.06, ' '],
+		[.11, '\u001b[32mW'],
+		[.12, 'o'],
+		[.13, 'r'],
+		[.14, 'l'],
+		[.15, 'd'],
+		[.16, '!\u001b[0m']
+	]
+}
 
 test('createRenderer', (t) => {
-	// todo
+	t.plan(2)
+	const render = createRenderer({width: asciicast.width, height: asciicast.height})
+
+	let svg = ''
+	for (let [delay, data] of asciicast.stdout) {
+		svg = render(data)
+	}
+
+	t.strictEqual(typeof svg, 'string')
+	t.ok(svg.indexOf('Hello World!') >= 0)
 })
